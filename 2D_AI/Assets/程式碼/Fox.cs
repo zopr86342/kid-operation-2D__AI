35aed077da597eac78f062962409bde0ffca5c58
zopr86342 <55218489+zopr86342@users.noreply.github.com>
2019-09-26 11:11:09 +0800

    認識C# 腳本與添加註解

---
+++ 2D_AI/Assets/程式碼/Fox.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;  //引用 Unity API- API=>倉庫 功能 、 工具
+
+public class Fox : MonoBehaviour  //類別 類別名稱
+{
+    //成員:欄位、屬性、方法、事件
+    //事件:在特定時間點會以指定頻率執行的方法
+    //開始事件:遊戲開始時執行一次(下面的
+    private void Start()
+    {
+        
+    }
+}
