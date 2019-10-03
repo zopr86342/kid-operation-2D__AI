c8aa65e633c0f5ef338c04fe0e82e6522e38534c
zopr86342 <55218489+zopr86342@users.noreply.github.com>
2019-10-03 11:51:34 +0800

    NPC 欄位設定

---
+++ 2D_AI/Assets/程式碼/F2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F2 : MonoBehaviour {
+    [Header("開頭對話")]
+    public string saystart = "嗨~你這隻死狐狸";
+    [Header("說話速度")]
+    public float sayspeed = 1.0f;
+    [Header("是否完成任務")]
+    public bool task = false;
+    [Header("完成任務對話")]
+    public string goodtask = "挖啊~你怎麼這麼棒";
+    [Header("任務失敗務對話")]
+    public string badtask = "你這小廢物";
+    [Header("玩家取得道具數量")]
+    public int playerProp = 0;
+    [Header("道具需求數量")]
+    public int countProp = 0;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
